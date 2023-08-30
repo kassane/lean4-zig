116a57d81380c83247cae43c5c83b765c9586783
Matheus C. França <matheus-catarino@hotmail.com>
2023-08-30 15:01:48 -0300

    Update build.zig

--- build.zig
+++ build.zig
@@ -65,7 +65,7 @@ fn reverseFFI(b: *std.Build, info: struct { std.zig.CrossTarget, std.builtin.Opt
     if (exe.target.isWindows()) {
         exe.addCSourceFile(.{.file = .{.path = "examples/reverse-ffi/lib/build/ir/RFFI.c" }, .flags = &.{}});
         //exe.linkSystemLibraryName("RFFI.dll"); // not found "libRFFI.dll.a"
-        exe.linkSystemLibraryName("libleanshared");
+        exe.linkSystemLibraryName("libleanshared.dll");
     } else {
         exe.linkSystemLibrary("RFFI");
         exe.linkSystemLibrary("leanshared");
