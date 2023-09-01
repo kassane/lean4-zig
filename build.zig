ef50047739b4db3abdaa12623645292267c86176
Matheus Catarino França <matheus-catarino@hotmail.com>
2023-09-01 10:39:47 -0300

    static-linking

--- build.zig
+++ build.zig
@@ -4,6 +4,8 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const shared = b.option(bool, "Shared", "Linking with libleanshared [default: true]") orelse true;
+
     _ = b.addModule("lean4", .{
         .source_file = .{
             .path = "src/c.zig",
@@ -11,7 +13,14 @@ pub fn build(b: *std.Build) !void {
     });
     lean4FFI(b);
     try runTest(b, target);
-    try reverseFFI(b, .{ target, optimize });
+    try reverseFFI(b, .{
+        .target = target,
+        .optimize = optimize,
+        .linkage = switch (shared) {
+            true => .dynamic,
+            false => .static,
+        },
+    });
 }
 
 fn lean4FFI(b: *std.Build) void {
@@ -31,12 +40,12 @@ fn lean4FFI(b: *std.Build) void {
     run_cmd.dependOn(&run.step);
 }
 
-fn reverseFFI(b: *std.Build, info: struct { std.zig.CrossTarget, std.builtin.OptimizeMode }) !void {
+fn reverseFFI(b: *std.Build, info: BuildInfo) !void {
     const exe = b.addExecutable(.{
         .name = "reverse-ffi",
         .root_source_file = .{ .path = "examples/reverse-ffi/app/app.zig" },
-        .target = info[0],
-        .optimize = info[1],
+        .target = info.target,
+        .optimize = info.optimize,
     });
     exe.addModule("lean4", b.modules.get("lean4").?);
     exe.addLibraryPath(.{ .path = "examples/reverse-ffi/lib/build/lib" });
@@ -52,11 +61,22 @@ fn reverseFFI(b: *std.Build, info: struct { std.zig.CrossTarget, std.builtin.Opt
 
     // static obj
     exe.addCSourceFile(.{ .file = .{ .path = "examples/reverse-ffi/lib/build/ir/RFFI.c" }, .flags = &.{} });
-    if (exe.target.isWindows()) {
-        exe.linkSystemLibraryName("leanshared.dll");
+
+    if (exe.target.isLinux() and info.linkage == .static) {
+        exe.linkSystemLibrary("leancpp");
+        exe.linkSystemLibrary("leanrt");
+        exe.linkSystemLibrary("Init");
+        exe.linkSystemLibrary("Lean");
+        exe.linkSystemLibrary("gmp");
+        exe.linkLibCpp(); // libc++ + libunwind + libc
     } else {
-        // exe.linkSystemLibrary("RFFI"); // sharedlib
-        exe.linkSystemLibrary("leanshared");
+        if (exe.target.isWindows()) {
+            // search library name - no pkg-config
+            exe.linkSystemLibraryName("leanshared.dll");
+        } else {
+            // detect library w/ pkg-config
+            exe.linkSystemLibrary("leanshared");
+        }
     }
     exe.linkLibC();
 
@@ -100,27 +120,33 @@ fn lean4Prefix(b: *std.Build) ![]const u8 {
 }
 
 fn runTest(b: *std.build, target: std.zig.CrossTarget) !void {
-    const main_tests = b.addTest(.{
+    const libTests = b.addTest(.{
         .name = "lean_test",
         .target = target,
         .optimize = .Debug,
         .root_source_file = .{ .path = "src/c.zig" },
     });
     const lib_dir = lean4LibDir(b, try lean4Prefix(b));
-    main_tests.addLibraryPath(.{ .path = lib_dir });
+    libTests.addLibraryPath(.{ .path = lib_dir });
 
-    if (main_tests.target.isDarwin()) {
-        main_tests.addLibraryPath(.{ .path = "/usr/local/lib" });
+    if (libTests.target.isDarwin()) {
+        libTests.addLibraryPath(.{ .path = "/usr/local/lib" });
     }
-    if (main_tests.target.isWindows()) {
-        main_tests.linkSystemLibraryName("leanshared.dll");
+    if (libTests.target.isWindows()) {
+        libTests.linkSystemLibraryName("leanshared.dll");
     } else {
-        main_tests.linkSystemLibrary("leanshared");
+        libTests.linkSystemLibrary("leanshared");
     }
-    main_tests.linkLibC();
-    const run_main_tests = b.addRunArtifact(main_tests);
-    if (main_tests.target.isWindows()) run_main_tests.addPathDir(lib_dir);
+    libTests.linkLibC();
+    const run_libTests = b.addRunArtifact(libTests);
+    if (libTests.target.isWindows()) run_libTests.addPathDir(lib_dir);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    test_step.dependOn(&run_libTests.step);
 }
+
+const BuildInfo = struct {
+    linkage: std.Build.Step.Compile.Linkage,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.OptimizeMode,
+};
